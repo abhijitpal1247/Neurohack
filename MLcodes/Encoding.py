ad2ccd5207056600e9e8fcf578c19959c0259880
ee17b031-iittp <ee17b031@iittp.ac.in>
2022-08-24 10:54:26 +0530

    Added word2vec model to encoding.py

--- MLcodes/Encoding.py
+++ MLcodes/Encoding.py
@@ -1,6 +1,12 @@
 import warnings
+from nltk.tokenize import sent_tokenize, word_tokenize
+from gensim.models import Word2Vec
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+ 
+warnings.filterwarnings(action = 'ignore')
+
 
 def countvectorizer(corpus, stopwords, lowercase, ngram_range, max_features):
     vectorizer = CountVectorizer(stop_words=stopwords, lowercase=lowercase, 
@@ -13,3 +19,20 @@ def tfidfvectorizer(corpus, stopwords, lowercase, ngram_range, max_features):
     ngram_range=ngram_range, max_features=max_features)
     vectorizer.fit(corpus)
     return vectorizer
+
+def word2vecmodel(corpus, stopwords, lowercase, skip_gram, vector_size, window_size):
+    data = []
+    # iterate through each sentence in the file
+    for i in corpus:
+        temp = []
+        # tokenize the sentence into words
+        for j in word_tokenize(i):
+            if lowercase:
+                if j.lower() not in stopwords:
+                    temp.append(j.lower())
+            else:
+                if j not in stopwords:
+                    temp.append(j)
+        data.append(temp)
+    model = Word2Vec(data, min_count=1, vector_size=vector_size, sg=skip_gram, window=window_size)
+    return model
