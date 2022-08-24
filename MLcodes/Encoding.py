bb66fd148ee8532ea80002afe67dc94ed546bd78
ee17b031-iittp <ee17b031@iittp.ac.in>
2022-08-24 09:58:43 +0530

    Added countvectorizer and tfidfvectorizer to encoding.py

---
+++ MLcodes/Encoding.py
@@ -0,0 +1,14 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def countvectorizer(corpus, stopwords, lowercase, ngram_range, max_features):
+    vectorizer = CountVectorizer(stop_words=stopwords, lowercase=lowercase, 
+    ngram_range=ngram_range, max_features=max_features)
+    vectorizer.fit(corpus)
+    return vectorizer
+
+def tfidfvectorizer(corpus, stopwords, lowercase, ngram_range, max_features):
+    vectorizer = TfidfVectorizer(stop_words=stopwords, lowercase=lowercase, 
+    ngram_range=ngram_range, max_features=max_features)
+    vectorizer.fit(corpus)
+    return vectorizer
